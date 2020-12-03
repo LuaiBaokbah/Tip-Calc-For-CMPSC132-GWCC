9a2473e5d080af58c166c637300f13fe2a7c6a5c
Luai Baokbah <Baokbah.luai@gmail.com>
2020-12-03 15:42:48 -0500

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Luai Baokbah on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
